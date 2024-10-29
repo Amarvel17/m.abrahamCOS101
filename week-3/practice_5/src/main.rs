701a023b19bb94631ea8c726559740cc21eb34de
Amarvel17- <marvelous.abraham@pau.edu.ng>
2024-10-29 16:30:51 +0100

    (My third commit)

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_character = '@'; //default
+    let alphabet:char = 'A';
+    let surname = "Abraham";
+    let first_name = "Marvelous";
+    let middle_name = "Moyinoluwa";
+
+    println!(" ");
+    println!("Special character: {}",special_character);
+    println!("Alphabet: {}",alphabet);
+    println!("Name: {} {} {}",surname, first_name, middle_name);
+}
