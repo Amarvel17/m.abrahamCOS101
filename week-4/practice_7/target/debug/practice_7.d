1e2e76c43d19d9e581d3a46e99dc37d00d591637
Amarvel17- <marvelous.abraham@pau.edu.ng>
2024-11-07 17:57:30 +0100

    (My fourth commit)

---
+++ week-4/practice_7/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\ISMS\Documents\m.abrahamCOS101\week-4\practice_7\target\debug\practice_7.exe: C:\Users\ISMS\Documents\m.abrahamCOS101\week-4\practice_7\src\main.rs
---
+++ week-4/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\ISMS\Documents\m.abrahamCOS101\week-4\practice_8\target\debug\practice_8.exe: C:\Users\ISMS\Documents\m.abrahamCOS101\week-4\practice_8\src\main.rs
